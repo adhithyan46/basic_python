8f979edb6605fb08f7ea975b46cd6718fc3cfaf9
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-10 11:21:58 +0530

    Add files via upload

---
+++ gnrtr_itr.py
@@ -0,0 +1,30 @@
+# def func():#defining a generator function
+#     yield 1
+#     yield 2
+#     yield 3
+# x=func()
+# for i in x:
+#     print(i)
+
+
+
+# def fun():
+#     for i in range(20):
+#         if (i%2==0):
+#             yield i
+# x=fun()
+# for i in x:
+#     print(i)
+
+#iterator
+
+# tup=("apple","orange","kiwi")
+# tup="adhithyan"
+# ite=iter(tup)
+# print(next(ite))
+# print(next(ite))
+# print(next(ite))
+# print(next(ite))
+
+
+   
