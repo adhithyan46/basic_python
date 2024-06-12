025c988ae4d6dbff058799fc8726f27fd1006352
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-11 22:27:56 -0700

    assnmt2.py

--- assnmt2.py
+++ assnmt2.py
@@ -158,4 +158,65 @@ print(k)
 
 list1=["red","green","apple","orange"]
 dict1={word:len(word) for word in list1}
-print(dict1)            
+print(dict1)  
+
+#14 dictionary concatination
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4={}
+dic1.update(dic2)
+dic1.update(dic3)
+dic4.update(dic1)
+print(dic4)
+
+#15 iteration over dictionary
+
+dic1={
+    1:10,
+    2:20,
+    3:30
+}
+for key,value in dic1.item():
+    print(f"{key}:{value}")
+
+#16sum of values in a dict
+
+dic1={
+     1:10,
+     2:20,
+     3:30
+ }
+dic_sum=sum(dic1.values())
+print(dic_sum)
+
+#17 dict
+
+pets={
+    "dog":"kuttu",
+    "cat":"wikky",
+    "cow":"drake"
+}
+# print(pets)
+for pet,name in pets.items():
+    print(f"{name} is a {pet}")
+
+#18
+
+def mark(mark):
+    hig_marks={}
+    for s,m in mark.items():
+        if m >50:
+         hig_marks[s]=m
+    return hig_marks
+marks = {
+        "English": 40,
+             "Maths": 60, 
+             "Hindi": 30,
+             "Chemistry": 46,
+             "Physics": 70
+             }
+highest=mark(marks)
+print("subject having marks more than 50",highest)
+
