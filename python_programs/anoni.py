b4411e8aa5eeb549c60726a9f020461492cf5d20
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-12 23:23:30 -0700

    Create anoni.py

---
+++ python_programs/anoni.py
@@ -0,0 +1,22 @@
+
+# add=lambda x,y:x+y
+# print(add(3,4))
+
+#map
+
+# list1=[1,2,3,4,5,6]
+# sqr=list(map(lambda x:x**2,list1))
+# print(sqr)
+
+#filter
+
+# list1=[1,2,3,4,5,6]
+# even=list(filter(lambda x:x%2!=0,list1))
+# print(even)
+
+#reduce
+
+# list1=[1,2,3,4]
+# from functools import reduce
+# prdct=reduce(lambda x,y:x*y,list1)
+# print(prdct) 
