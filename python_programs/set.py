a134021660872e9b0f73a88347d24c172810f04f
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-12 23:30:08 -0700

    set.py

---
+++ python_programs/set.py
@@ -0,0 +1,19 @@
+#set1={"red","green","blue","yellow","red",1}
+# for i in set1:
+# print("red" not in set1)
+# set2=("kiwi")
+#set2={2,3,4,True,"red"}
+# list1=[4,3,2,1]
+# set1.update(list1)
+# set1.discard("yellow")
+# set3=set1.union(set2)
+#set3=set1 | set2
+# set3=set1.intersection(set2)
+# set3=set1 & set2
+#set3=set1.difference(set2)
+# set3=set1-set2
+#set1.difference_update(set2)
+# set3=set1.symmetric_difference(set2)
+# set3=set1^set2
+# x=set1.isjoint(set2)
+# print(x)
