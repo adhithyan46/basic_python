a524a185c97db9abf521b23c38251e67f352d159
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-12 23:29:04 -0700

    loop.py

---
+++ python_programs/loop.py
@@ -0,0 +1,32 @@
+#for
+# n=[1,2,3,4,5]
+# for i in n:
+#     print(i)
+
+#while
+
+# n=1
+# while n<=10:
+#     print(n)
+#     n+=1
+
+
+# for i in range(1,11):
+#     print(i)
+
+
+#control stmt
+
+#break
+
+# for i in range(1,11):
+#     if i==6:
+#         break
+#     print(i)
+
+#continue
+
+for i in range(1,11):
+    if i==5:
+        continue  
+    print(i)
