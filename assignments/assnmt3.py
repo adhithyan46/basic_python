b050b0f9915c211611d6d282145d8ea05db2843f
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-13 00:26:53 -0700

    assnmt3.py

---
+++ assignments/assnmt3.py
@@ -0,0 +1,30 @@
+##1
+
+# class string:
+#     def __init__(self):
+#         self.input_str=""
+#     def str1(self):
+#      self.input_str=input("enter a string:")
+       
+#     def cap(self):
+#         print(self.input_str.upper()) 
+        
+# s=string()
+# s.str1()
+# s.cap()
+
+##2
+
+# class para:
+   
+#   parmeter="hello world !"
+#   def __init__(self):
+#       self.msg="hello world !"
+#   def parameters(self):
+#      print(f"class parametr:{para.parmeter}")
+#      print(f"instance parametre:{self.msg}")
+         
+    
+# c=para()
+# c.parameters()
+      
