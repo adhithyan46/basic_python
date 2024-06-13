a01af846acec8e49cff167cb19612e3f03c79b4c
adhithyan46 <ksadhithyan100@gmail.com>
2024-06-13 22:03:39 +0530

    assnmt3.py

--- assignments/assnmt3.py
+++ assignments/assnmt3.py
@@ -27,4 +27,51 @@
     
 # c=para()
 # c.parameters()
-      
+
+##3 area of a circle
+
+# class circle:
+    
+#     def __init__(self):
+#         self.radius=""
+#     def rad(self):
+#         self.radius=int(input("enter the radius:"))
+#     def area(self):
+#         from math import pi
+#         print("Area of the circle=",pi*self.radius**2)
+# c=circle()
+# c.rad()
+# c.area()
+
+##bank account
+
+class bank_ant:
+    def __init__(self):
+        self.dpst=""
+        self.wd=""
+    def deposit(self):
+        self.dpst=int(input("Enter the amount you deposited:"))
+    def withdraw(self):
+        self.wd=int(input("Enter the withdrawed amount:"))
+    def balance(self):
+        print("The balance amount in your bank account is",self.dpst - self.wd)
+b=bank_ant()
+b.deposit()
+b.withdraw()
+b.balance()
+
+##area of the square
+
+class shape:
+    def __init__(self):
+       pass
+    def area(self):
+        self.area=0
+class square(shape):
+    def __init__(self, length):
+        self.length=length
+        
+    def area(self):
+        print("area of the square=",self.length**2)
+s=square(2)
+s.area()
